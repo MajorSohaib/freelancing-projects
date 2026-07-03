19a93bb4994a6a62c375fd53efa6b80b13dbcdb9
Sohaib Tahir <sohaibtahir9870@gmail.com>
2026-07-03 14:56:20 +0500

    Day 2: quotes scraper updated with tags, saved to CSV

--- scraping/quotes_scraper.py
+++ scraping/quotes_scraper.py
@@ -10,7 +10,11 @@ data = []
 for quote in quotes:
     text = quote.find("span", class_="text").get_text()
     author = quote.find("small", class_="author").get_text()
-    data.append({"quote": text, "author": author})
+    tag = quote.find_all("a", class_="tag")
+    tag_list = [t.get_text() for t in tag]
+    data.append({"quote": text, "author": author, "tag":tag_list})
+    
+   
 
 df = pd.DataFrame(data)
 df.to_csv("scraping/quotes.csv", index=False)
