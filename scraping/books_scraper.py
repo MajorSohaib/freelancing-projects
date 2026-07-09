4a44028291aa52796f35f818ac815a791b57af2b
Sohaib Tahir <sohaibtahir9870@gmail.com>
2026-07-09 11:02:59 +0500

    Day 2: Multi-page scraper complete, 1000 books scraped to CSV

--- scraping/books_scraper.py
+++ scraping/books_scraper.py
@@ -9,12 +9,18 @@ books = soup.find_all("article", class_="product_pod")
 
 data = []
 
-for book in books:
-    h3 = book.find("h3")
-    name = h3.find("a")["title"]
-    price = book.find("p", class_="price_color").get_text()
-    rating = book.find("p", class_="star-rating")["class"][1]
-    data.append({"title": name, "price": price, "rating": rating})
+for i in range(1, 51):
+    url = f"http://books.toscrape.com/catalogue/page-{i}.html"
+    response = requests.get(url)
+    soup  = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article", class_="product_pod")
+    
+    for book in books:   
+        h3 = book.find("h3")
+        name = h3.find("a")["title"]
+        price = book.find("p", class_="price_color").get_text()
+        rating = book.find("p", class_="star-rating")["class"][1]
+        data.append({"title": name, "price": price, "rating": rating})
     
     
 import pandas as pd
