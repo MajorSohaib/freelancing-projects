d976360d0b664508f36891d79b5979d910553077
Sohaib Tahir <sohaibtahir9870@gmail.com>
2026-07-09 12:02:47 +0500

    Day 2: quotes scraper updated to scrape all 10 pages, 100 quotes saved

--- scraping/quotes_scraper.py
+++ scraping/quotes_scraper.py
@@ -1,18 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-url = "http://quotes.toscrape.com"
+"""url = "http://quotes.toscrape.com"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-quotes = soup.find_all("div", class_="quote")
+quotes = soup.find_all("div", class_="quote")"""
 
 data = []
-for quote in quotes:
-    text = quote.find("span", class_="text").get_text()
-    author = quote.find("small", class_="author").get_text()
-    tag = quote.find_all("a", class_="tag")
-    tag_list = [t.get_text() for t in tag]
-    data.append({"quote": text, "author": author, "tag":tag_list})
+
+for i in range(1, 11):
+    url = f"http://quotes.toscrape.com/page/{i}/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    quotes = soup.find_all("div", class_="quote")
+    
+    for quote in quotes:
+        text = quote.find("span", class_="text").get_text()
+        author = quote.find("small", class_="author").get_text()
+        tag = quote.find_all("a", class_="tag")
+        tag_list = [t.get_text() for t in tag]
+        data.append({"quote": text, "author": author, "tag":tag_list})
     
    
 
