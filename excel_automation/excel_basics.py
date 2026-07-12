a995a5eab76e46436d1afbca83137feed34ef54d
Sohaib Tahir <sohaibtahir9870@gmail.com>
2026-07-12 13:10:31 +0500

    Day 4: Excel files with bold letters and column with complete

--- excel_automation/excel_basics.py
+++ excel_automation/excel_basics.py
@@ -1,11 +1,20 @@
 import openpyxl
 import pandas as pd
+from openpyxl.styles import Font
 
 wb = openpyxl.Workbook()
 ws = wb.active
 ws.title = "Books Data"
-ws.append (["Title", "Price", "Rating"])
+header_font = Font(bold=True, size=12)
 
+ws.append (["Title", "Price", "Rating"])
+for cell in ws[1]:
+    cell.font = header_font
+    
+ws.column_dimensions["A"].width = 60
+ws.column_dimensions["B"].width = 15
+ws.column_dimensions["C"].width = 15
+    
 df = pd.read_csv("scraping/books.csv")
 
 for index, row in df.iterrows():
